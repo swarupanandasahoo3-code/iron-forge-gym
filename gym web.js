cf00759264bf10bde81464b0da81fae452e7813e
Swarupananda Sahoo <swarupanandasahoo3@gmail.com>
2026-04-11 20:13:53 +0530

    Add files via upload

---
+++ gym web.js
@@ -0,0 +1,36 @@
+
+window.addEventListener(`scroll`, () => {
+    const nav = document.querySelector(`nav`);
+
+    if (window.scrollY > 50){
+        nav.classList.add(`scrolled`);
+    }
+
+    else {
+        nav.classList.remove(`scrolled`)
+    }
+})
+
+const hiddenElements = document.querySelectorAll(`.hidden`)
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add(`show`);
+        }
+    });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+function toggleCard(card){
+    let allCards = document.querySelectorAll(`.service-card`, `.why-card`);
+
+    allCards.forEach(c => {
+        if(c != card){
+            c.classList.remove(`active`);
+        }
+    })
+
+    card.classList.toggle(`active`);
+}
